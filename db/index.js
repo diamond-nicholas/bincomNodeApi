7b7ce76a4fbf123a215ae10cb5b82b3eef70362e
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-08-25 22:51:01 +0100

    fix pg bug

--- db/index.js
+++ db/index.js
@@ -1,5 +1,20 @@
-const { Pool } = require('pg');
-const pool = new Pool();
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString =
+  'postgres://ybhagwwk:xcTkmmGenqjUJgUSxJsVsvrlgxzHg55D@otto.db.elephantsql.com/ybhagwwk'; //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function (err) {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
